9d1b4ccf2f18199d52ec007bab6de63003caa8da
sidd5449 <sidd5449@gmail.com>
2025-01-08 12:13:14 +0530

    minor changes

--- controllers/updateController.js
+++ controllers/updateController.js
@@ -1,14 +1,24 @@
 import userData from "../dbSchema/userData.js";
+import postData from '../dbSchema/postData.js'
 import { createCommit } from "../utils/createCommit.js";
 
+
+// fetched post data
+
+// add selective updated to post metadata
 export const updatePostController = async (req, res) => {
+
+    // api inputs:
+    // userId, fileId 
+    // file
     try {
         console.log(req.body)
         const data  = req.body;
         const user = await userData.find({ id: data.userId });
         console.log(user);
-        const postData = req.file;
-        createCommit(user[0].username, user[0].repoName, postData, user[0].token, data.filename)
+        const postFile = req.file;
+        const post = await postData.find({id: data.fileId});
+        createCommit(user[0].username, user[0].repoName, postFile, user[0].token, data.filename)
         res.status(204).json("Post Updated Successfully");
     } catch (err) {
         res.status(400).json({ message: err.message })
